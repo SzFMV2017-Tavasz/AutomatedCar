6a101c1e4df1c34e7f2cced9bcae0c4e56507a4b
pintergreg <pintergreg@gmail.com>
2017-02-13 10:17:38 +0100

    add test for Signal

---
+++ src/test/java/hu/oe/nik/automatedcar/bus/SignalTest.java
@@ -0,0 +1,24 @@
+package hu.oe.nik.automatedcar.bus;
+
+import static org.junit.Assert.assertEquals;
+
+public class SignalTest {
+
+	private Signal signal;
+
+	@org.junit.Before
+	public void setUp() throws Exception {
+		/* stuff written here runs before the tests */
+		signal = new Signal(0,42);
+	}
+
+	@org.junit.Test
+	public void testIdGetter(){
+		assertEquals(signal.getId(), 0);
+	}
+
+	@org.junit.Test
+	public void testDataGetter(){
+		assertEquals(signal.getData(), 42);
+	}
+}
