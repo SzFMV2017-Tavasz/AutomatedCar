d3161f93aafc86313cb74167c1646dc4cc9c9fa2
pintergreg <pintergreg@gmail.com>
2017-02-13 11:20:43 +0100

    add test for VirtualFunctionBus

---
+++ src/test/java/hu/oe/nik/automatedcar/bus/VirtualFunctionBusTest.java
@@ -0,0 +1,35 @@
+package hu.oe.nik.automatedcar.bus;
+
+import hu.oe.nik.automatedcar.SystemComponent;
+
+import static org.junit.Assert.assertEquals;
+
+public class VirtualFunctionBusTest {
+
+	@org.junit.Before
+	public void registerComponent(){
+		SystemComponentMock comp = new SystemComponentMock();
+		VirtualFunctionBus.registerComponent(comp);
+	}
+
+	@org.junit.Test
+	public void sendSignalTest(){
+		VirtualFunctionBus.sendSignal(new Signal(0,42));
+	}
+
+
+	class SystemComponentMock extends SystemComponent{
+
+		@Override
+		public void loop() {
+
+		}
+
+		@Override
+		public void receiveSignal(Signal s) {
+			if (s.getId() == 0){
+				assertEquals(s.getData(), 42);
+			}
+		}
+	}
+}
