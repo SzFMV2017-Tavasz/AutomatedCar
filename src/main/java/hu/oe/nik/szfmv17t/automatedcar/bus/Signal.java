7f63dafd15be4c73b7d2c1a3905af1baeae2714a
kande <kande@AnderPC>
2017-02-22 20:33:30 +0100

    First branch commit

--- src/main/java/hu/oe/nik/szfmv17t/automatedcar/bus/Signal.java
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/bus/Signal.java
@@ -15,7 +15,7 @@ package hu.oe.nik.szfmv17t.automatedcar.bus;
  *
  */
 
-//Test comment
+//Test comment for test branch
 public class Signal {
 	// Signal identifier, a component can decide based on this value
 	// whether the content of the signal shall be processed or not.
