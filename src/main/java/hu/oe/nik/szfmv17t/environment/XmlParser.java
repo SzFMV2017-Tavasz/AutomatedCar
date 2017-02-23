0ad3df5d6e90b9ad59ef83d5014fcc0b55f7dc5a
Gellért Bábel <galli.babel94@gmail.com>
2017-02-23 23:34:58 +0100

    Add XML parser class

---
+++ src/main/java/hu/oe/nik/szfmv17t/environment/XmlParser.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.oe.nik.szfmv17t.environment;
+
+import java.util.List;
+
+/**
+ *
+ * @author Gellert Babel <OE-NIK>
+ */
+public class XmlParser {
+    public void getWorldObjects(String pathToXml,List<WorldObject> worldObjects)
+    {
+        //Maybe remove worldObjects list if not needed.
+    }
+}
