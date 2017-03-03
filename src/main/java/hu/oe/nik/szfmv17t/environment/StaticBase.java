e55bf402746237fe3c93db2423022738c76340f0
Gellert Babel <gelli.babel94@gmail.com>
2017-03-03 14:10:50 +0100

    Adding skeleton for stationary objects.
    Maybe Turn should be part of Road.

---
+++ src/main/java/hu/oe/nik/szfmv17t/environment/StaticBase.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.oe.nik.szfmv17t.environment;
+
+/**
+ *
+ * @author Gellert Babel <OE-NIK>
+ */
+//Base of stationary objects
+public class StaticBase  extends WorldObjectBase{
+
+    @Override
+    public void updateWorldObject() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getZIndex() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public WorldObjectState state() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/main/java/hu/oe/nik/szfmv17t/environment/ZebraCrossing.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.oe.nik.szfmv17t.environment;
+
+/**
+ *
+ * @author Gellert Babel <OE-NIK>
+ */
+public class ZebraCrossing extends StaticBase {
+    
+}
