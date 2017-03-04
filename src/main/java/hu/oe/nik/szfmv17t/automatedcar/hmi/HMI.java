3883fbe82ac99ec2a0bdaab8df70d8e5fc38d051
I331205 <miklos.sebestyen@sap.com>
2017-03-04 20:40:58 +0100

    Hello Smi label added to CourseDisplay. preparing for keypress listener implementation

--- src/main/java/hu/oe/nik/szfmv17t/automatedcar/hmi/HMI.java
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/hmi/HMI.java
@@ -1,8 +1,26 @@
 package hu.oe.nik.szfmv17t.automatedcar.hmi;
 
+import com.sun.xml.internal.bind.v2.TODO;
+import hu.oe.nik.szfmv17t.automatedcar.ISystemComponent;
+import hu.oe.nik.szfmv17t.automatedcar.SystemComponent;
+import hu.oe.nik.szfmv17t.automatedcar.bus.Signal;
+
 /**
  * Created by SebestyenMiklos on 2017. 02. 26..
  */
-public class HMI {
+public class HMI extends SystemComponent {
 
+    public HMI() {
+        super();
+    }
+
+    @Override
+    public void loop() {
+        //TODO:send break gas and gear signals
+    }
+
+    @Override
+    public void receiveSignal(Signal s) {
+
+    }
 }
