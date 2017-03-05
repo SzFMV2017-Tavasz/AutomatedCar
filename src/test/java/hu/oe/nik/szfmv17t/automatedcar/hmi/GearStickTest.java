dfe1dcf950b6706eb5cf55bce653710531825e42
NyiroGabor <gabi108@gmail.com>
2017-03-05 15:23:39 +0100

    GearStick

---
+++ src/test/java/hu/oe/nik/szfmv17t/automatedcar/hmi/GearStickTest.java
@@ -0,0 +1,56 @@
+package hu.oe.nik.szfmv17t.automatedcar.hmi;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by gabi1_000 on 2017.03.05..
+ */
+public class GearStickTest {
+
+    private GearStick gearStick;
+
+
+    @Before
+    public void setUp() throws Exception {
+        gearStick = new GearStick(100);
+    }
+
+
+
+    @Test
+    public void getManualGearState() throws Exception {
+        int actualManualGear = gearStick.GetManualGearState();
+        assertTrue(actualManualGear >= 0 || actualManualGear <= 6);
+    }
+
+    @Test
+    public void getAutoGearState() throws Exception {
+        AutoGearStates currentState = gearStick.GetAutoGearState();
+        assertTrue(currentState == AutoGearStates.P || currentState == AutoGearStates.P || currentState == AutoGearStates.P);
+    }
+
+    @Test
+    public void gearDown() throws Exception {
+        gearStick.GearDown();
+        int currentManualGear = gearStick.GetManualGearState();
+        assertTrue(currentManualGear >= 0);
+    }
+
+    @Test
+    public void gearUp() throws Exception {
+        gearStick.GearUp();
+        int currentManualGear = gearStick.GetManualGearState();
+        assertTrue(currentManualGear <= 6);
+    }
+
+    @Test
+    public void setGearState() throws Exception {
+        gearStick.SetGearState(gearStick.GetAutoGearState());
+        AutoGearStates currentState = gearStick.GetAutoGearState();
+        assertTrue(currentState == AutoGearStates.P || currentState == AutoGearStates.P || currentState == AutoGearStates.P);
+    }
+
+}
