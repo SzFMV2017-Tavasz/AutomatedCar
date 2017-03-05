2b9c19c1cdd5194f49d683ebfe450eef3bb82556
Gellert Babel <gelli.babel94@gmail.com>
2017-03-05 13:29:42 +0100

    Move classes to folders

--- src/main/java/hu/oe/nik/szfmv17t/environment/utils/XmlParser.java
+++ src/main/java/hu/oe/nik/szfmv17t/environment/utils/XmlParser.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hu.oe.nik.szfmv17t.environment;
+package hu.oe.nik.szfmv17t.environment.utils;
 
 import java.util.List;
 
