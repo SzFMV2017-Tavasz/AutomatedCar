a7a468be1c19dcb65413c2e84372671d6b40b7d7
laligelencser <gele1993@gmail.com>
2017-03-07 10:17:39 +0100

    BrakePedal class rewrite

--- src/main/java/hu/oe/nik/szfmv17t/automatedcar/hmi/BrakePedal.java
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/hmi/BrakePedal.java
@@ -1,39 +1,64 @@
 package hu.oe.nik.szfmv17t.automatedcar.hmi;
 
 public class BrakePedal{
-    private int state;
+    public static final int MAX_STATE = 100;
+    public static final int MIN_STATE = 0;
+    public static final int START_STATE = 0;
+    public static final int DEFAULT_AMOUNT = 10;
+    public static final int LENGTH_OF_BUTONPRESS_TO_MAX_OR_MIN = 1000; // milisecond
+    
+    public HmiTimer timer;
     private int amount;
+    private int state;
     
     public BrakePedal(){
-        state = 0;
-        amount = 10;
+        state = START_STATE;
+        amount = DEFAULT_AMOUNT;
     }
     
-    public BrakePedal(int changeAmount){
-        state = 0;
-        amount = changeAmount;
+    public void braking(){
+        if (timer.getDuration() >= LENGTH_OF_BUTONPRESS_TO_MAX_OR_MIN) {
+            releaseBrake();
+        }
+        else{
+            decreaseBrake();
+        }
     }
     
-    public void IncreaseBrake(){
-        state += amount;
+    public void releasingBrake(){
+        if (timer.getDuration() >= LENGTH_OF_BUTONPRESS_TO_MAX_OR_MIN) {
+            increaseBrake();
+        }
+        else{
+            emergencyBrake();
+        }
     }
     
-    public void DecreaseBreak(){
-        state -= amount;
+    private void increaseBrake(){
+        if(state + amount <= MAX_STATE){
+            state += amount;
+        }
     }
     
-    public void EmergencyBreaking(){
+    private void decreaseBrake(){
+        if(state - amount >= MIN_STATE){
+            state -= amount;
+        }
+    }
+    
+    private void emergencyBrake(){
         state = 100;
     }
     
-    public void ReleaseBrake(){
+    private void releaseBrake(){
         state = 0;
     }
 
-    /**
-     * @return the state
-     */
     public int getState() {
         return state;
+    }
+    
+    public void start() {
+        timer.Start();
     }
 }
