e97801871daa8cbbb327ca93fc743de8af1782b2
Admin <Admin@Admin-PC>
2017-03-14 09:38:37 +0100

    minor changes

--- src/main/java/hu/oe/nik/szfmv17t/physics/Acceleration.java
+++ src/main/java/hu/oe/nik/szfmv17t/physics/Acceleration.java
@@ -9,22 +9,18 @@ public final class Acceleration {
 	private static double maxGearAcceleration;
 	private static double maxGearVelocity;
 	
-	private static double[] gear_max_acceleration={-10,0,20,18,15,10,5};
-	private static double[] gear_max_velocity={-10,0,20,18,15,10,5};
-	
+	//linear proportion
 	public static double CalculateAcceleration(int gearShift,double gaspedalPercentage)
 	{
 		setMaxGearAccelerationByGear(gearShift);
-		return gaspedalPercentage/100*maxGearAcceleration;
+		return gaspedalPercentage*maxGearAcceleration;
 	}
 	private static void setMaxGearAccelerationByGear(int gearShift)
 	{
-		//maxGearAcceleration=SpeedControl.GEAR_MAX_ACCELERATION[GEARSHIFT+1];
-		maxGearAcceleration=gear_max_acceleration[gearShift+1];
+		maxGearAcceleration=SpeedControl.GEAR_MAX_ACCELERATION[gearShift];
 	}
 	private static void setMaxGearVelocityByGear(int gearShift)
 	{
-		//maxGearVelocity=SpeedControl.GEAR_MAX_VELOCITY[GEARSHIFT+1];
-		maxGearVelocity=gear_max_velocity[gearShift+1];
+		maxGearVelocity=SpeedControl.GEAR_MAX_VELOCITY[gearShift];		
 	}
 }
