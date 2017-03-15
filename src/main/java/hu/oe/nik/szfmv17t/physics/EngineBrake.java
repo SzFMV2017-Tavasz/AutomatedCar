49f4b79e39c7b8e0588486f684296f46a5613a12
xpowerfullx <xpowerfulx@gmail.com>
2017-03-15 15:02:37 +0100

    added new gear EngineBrake implemented

--- src/main/java/hu/oe/nik/szfmv17t/physics/EngineBrake.java
+++ src/main/java/hu/oe/nik/szfmv17t/physics/EngineBrake.java
@@ -1,18 +1,36 @@
 package hu.oe.nik.szfmv17t.physics;
 
 public class EngineBrake {
-
-	float deceleration;
 	
-	private void setDecelerationByGear(int gear){
-	deceleration=-10;	
-	}
+	public double calculateAcceleration(int gear, float gasPedalPercentage, double actualVelocity) {
+		
+		double deceleration = 0;
 
-	public float calculateAcceleration(int gear, float gasPedalPercentage){
-		double s = SpeedControl.GEAR_MAX_ACCELERATION[gear];
-		double t = SpeedControl.GEAR_MAX_VELOCITY[gear];
+		if (gear >0 && gasPedalPercentage == 0 && actualVelocity>0) {
 
-		return 0;
+			double startInterval = SpeedControl.GEAR_MAX_VELOCITY[gear];
+			double endInterval = SpeedControl.GEAR_MAX_VELOCITY[gear+1];
+			double gearVelocityInterval = endInterval - startInterval;
+			
+			int ratio=(gear+1)*4;
+			
+			if (actualVelocity >= startInterval && actualVelocity <= startInterval + gearVelocityInterval * 0.3)
+				deceleration = -gearVelocityInterval / ratio;
+			else if (actualVelocity > startInterval + gearVelocityInterval * 0.3
+					&& actualVelocity <= startInterval + gearVelocityInterval * 0.7)
+				deceleration = -gearVelocityInterval / ratio;
+			else
+				deceleration = -gearVelocityInterval / ratio;
+		}
+		else if(gear==-1 && gasPedalPercentage == 0 && actualVelocity>0 ){
+			double interval = SpeedControl.GEAR_MAX_VELOCITY[0];
+			if(actualVelocity >0 && actualVelocity <= interval*0.3)
+				deceleration= -interval/20;
+			else if(actualVelocity >interval*0.3 && actualVelocity <= interval*0.7)
+				deceleration = -interval/15;
+			else
+				deceleration=-interval/12;
+		}
+		return deceleration;
 	}
 }
-
