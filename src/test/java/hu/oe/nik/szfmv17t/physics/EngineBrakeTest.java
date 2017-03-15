215e33fba5c74b32d1f1e7f821869c79d5a85bd1
tothc <tothcs1105@gmail.com>
2017-03-15 14:53:48 +0100

    ExternalForces and EngineBrake added to SpeedControl as fields, their calculateAcceleration method added to SpeedControl.sumAcceleration method.

--- src/test/java/hu/oe/nik/szfmv17t/physics/EngineBrakeTest.java
+++ src/test/java/hu/oe/nik/szfmv17t/physics/EngineBrakeTest.java
@@ -1,15 +1,13 @@
 package hu.oe.nik.szfmv17t.physics;
-import org.junit.Test;
 
 public class EngineBrakeTest {
 
-	@Test
-	public void CalculateAcceleration(int gear, float gasPedalPercentage)
-	{
-		EngineBrake brake = new EngineBrake();
-		
-		
-		
-	}
-	
+	/*
+	 * @Test public void CalculateAcceleration(int gear, float
+	 * gasPedalPercentage) { EngineBrake brake = new EngineBrake();
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
 }
