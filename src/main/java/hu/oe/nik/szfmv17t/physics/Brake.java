4e2d82c4ea29ad3fe37554c643b94eecc0156ae0
xpowerfullx <xpowerfulx@gmail.com>
2017-03-15 22:35:42 +0100

    fixed underscores, capital letters

--- src/main/java/hu/oe/nik/szfmv17t/physics/Brake.java
+++ src/main/java/hu/oe/nik/szfmv17t/physics/Brake.java
@@ -4,7 +4,7 @@ public class Brake {
 
 	private final double MAX_BRAKING_DECELERATION = -9.5; // m/s^2
 	
-	public double CalculateAcceleration(double brakePedalPercentage){
+	public double calculateAcceleration(double brakePedalPercentage){
 		double acceleration = 0;
 		acceleration = MAX_BRAKING_DECELERATION * brakePedalPercentage;
 		return acceleration;
