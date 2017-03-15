8dda164f96082e842e3e1dd4894043fe36005888
xpowerfullx <xpowerfulx@gmail.com>
2017-03-15 21:45:30 +0100

    fixed underscores

--- src/test/java/hu/oe/nik/szfmv17t/physics/EngineBrakeTest.java
+++ src/test/java/hu/oe/nik/szfmv17t/physics/EngineBrakeTest.java
@@ -1,27 +1,26 @@
 package hu.oe.nik.szfmv17t.physics;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class EngineBrakeTest {
 
 	@Test
-	public void calculateAcceleration_ShouldReturn_NegativeAcceleration_WhenCalledWith_OneToFiveGears_Test1(){
+	public void calculateAccelerationShouldReturnNegativeAccelerationWhenCalledWithOneToFiveGearsTest1(){
 		EngineBrake eb = new EngineBrake();
 		double dec = eb.calculateAcceleration(1, 0, 2);
 		assertEquals(-0.5,dec,0);
 	}
 	
 	@Test
-	public void calculateAcceleration_ShouldReturn_NehativeAcceleration_WhenCalledWith_OneToFiveGears_Test2(){
+	public void calculateAccelerationShouldReturnNehativeAccelerationWhenCalledWithOneToFiveGearsTest2(){
 		EngineBrake eb = new EngineBrake();
 		double dec = eb.calculateAcceleration(1, 0, 4);
 		assertEquals(-0.6875,dec,0);
 	}
 	
 	@Test
-	public void calculateAcceleration_ShouldReturn_NegativeAcceleration_WhenCalledWith_MinusOneGear(){
+	public void calculateAccelerationShouldReturnNegativeAccelerationWhenCalledWithMinusOneGear(){
 		EngineBrake eb = new EngineBrake();
 		double acc = eb.calculateAcceleration(-1, 0, 4);
 		double expected = -((double)4/12);
