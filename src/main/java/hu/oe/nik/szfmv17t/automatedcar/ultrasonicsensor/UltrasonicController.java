bac278bfc0bbd503e7634398c531195fc7e77183
SebestyenMiklos <sebestyenmiklos1neptun@gmail.com>
2017-03-26 12:54:44 +0200

    Ultrasonic constructor added

--- src/main/java/hu/oe/nik/szfmv17t/automatedcar/ultrasonicsensor/UltrasonicController.java
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/ultrasonicsensor/UltrasonicController.java
@@ -2,11 +2,18 @@ package hu.oe.nik.szfmv17t.automatedcar.ultrasonicsensor;
 
 import hu.oe.nik.szfmv17t.automatedcar.SystemComponent;
 import hu.oe.nik.szfmv17t.automatedcar.bus.Signal;
+import hu.oe.nik.szfmv17t.automatedcar.hmi.HMI;
+import hu.oe.nik.szfmv17t.environment.domain.World;
 
 /**
  * Created by SebestyenMiklos on 2017. 03. 26..
  */
 public class UltrasonicController extends SystemComponent {
+
+    public UltrasonicController(IUltrasonicSensorController hmi, World w) {
+
+    }
+
     @Override
     public void loop() {
 
