61698fe19de08ef275abccdee34a7199861af37e
SebestyenMiklos <sebestyenmiklos1neptun@gmail.com>
2017-03-26 12:45:49 +0200

    Ultrasonic sensor skeleton classes added

---
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/ultrasonicsensor/UltrasonicController.java
@@ -0,0 +1,19 @@
+package hu.oe.nik.szfmv17t.automatedcar.ultrasonicsensor;
+
+import hu.oe.nik.szfmv17t.automatedcar.SystemComponent;
+import hu.oe.nik.szfmv17t.automatedcar.bus.Signal;
+
+/**
+ * Created by SebestyenMiklos on 2017. 03. 26..
+ */
+public class UltrasonicController extends SystemComponent {
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void receiveSignal(Signal s) {
+
+    }
+}
