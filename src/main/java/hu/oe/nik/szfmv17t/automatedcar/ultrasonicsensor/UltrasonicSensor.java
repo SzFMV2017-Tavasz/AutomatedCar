0e3c526e4cd85d403aa5cb84269d771dc5633bd7
kander794 <k.ander794@gmail.com>
2017-03-26 22:00:09 +0200

    implement skeleton for UltrasonicController and UltrasonicSensor classes

--- src/main/java/hu/oe/nik/szfmv17t/automatedcar/ultrasonicsensor/UltrasonicSensor.java
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/ultrasonicsensor/UltrasonicSensor.java
@@ -1,7 +1,30 @@
 package hu.oe.nik.szfmv17t.automatedcar.ultrasonicsensor;
+import hu.oe.nik.szfmv17t.environment.interfaces.ICollidableObject;
+import hu.oe.nik.szfmv17t.environment.utils.Position;
 
 /**
  * Created by SebestyenMiklos on 2017. 03. 26..
  */
 public class UltrasonicSensor {
+	private Position coordinates;
+	private int viewLength;
+	private Position viewLeftCoordinate;
+	private Position viewRightCoordinate;
+	private int viewAngle;
+	
+	public UltrasonicSensor(int sensorNumber,UltrasonicController controller){
+		
+	}
+	public ICollidableObject getClosestWorldObject(){
+		return null;
+	}
+	private int calculateObjectExtension(Object object){
+		return 0;
+	}
+	private Boolean isWorldObjectDetectable(ICollidableObject colladibleObject){
+		return true;
+	}
+	private void calculateCoordinates(){
+		
+	}
 }
