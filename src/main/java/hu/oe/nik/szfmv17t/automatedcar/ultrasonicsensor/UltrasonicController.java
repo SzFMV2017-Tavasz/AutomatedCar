c76edc26a24ceaa6bd41a40267c70ad368f537f7
NyiroGabor <gabi108@gmail.com>
2017-04-02 17:13:26 +0200

    Creating all of the sensors with their basic positions when the simulation starts

--- src/main/java/hu/oe/nik/szfmv17t/automatedcar/ultrasonicsensor/UltrasonicController.java
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/ultrasonicsensor/UltrasonicController.java
@@ -21,10 +21,26 @@ public class UltrasonicController extends SystemComponent {
 		this.automatedCar = auto;
 		initSensors();
 	}
+
+	// Alapértelmezetten autó felfele néz, óramutató járásával megegyezően vannak megszámozva a szenzorok
+	// Ennek megfelelően autó jobb felső sarkánál az előre néző szenzor az 1-es számú
 	private void initSensors(){
-		ultrasonicSensors = new ArrayList<UltrasonicSensor>();
-		
+		ultrasonicSensors = new ArrayList<UltrasonicSensor>(){
+			UltrasonicSensor US1 = new UltrasonicSensor(1, automatedCar.getCenterX()+15, automatedCar.getCenterY()-39);
+			UltrasonicSensor US2 = new UltrasonicSensor(2, automatedCar.getCenterX()+17, automatedCar.getCenterY()-37);
+			UltrasonicSensor US3 = new UltrasonicSensor(3, automatedCar.getCenterX()+17, automatedCar.getCenterY()+37);
+			UltrasonicSensor US4 = new UltrasonicSensor(4, automatedCar.getCenterX()+15, automatedCar.getCenterY()+39);
+			UltrasonicSensor US5 = new UltrasonicSensor(5, automatedCar.getCenterX()-15, automatedCar.getCenterY()+39);
+			UltrasonicSensor US6 = new UltrasonicSensor(6, automatedCar.getCenterX()-17, automatedCar.getCenterY()+37);
+			UltrasonicSensor US7 = new UltrasonicSensor(7, automatedCar.getCenterX()-17, automatedCar.getCenterY()-37);
+			UltrasonicSensor US8 = new UltrasonicSensor(8, automatedCar.getCenterX()-15, automatedCar.getCenterY()-39);
+		};
 	}
+
+	public void sensorPositionRefresh(AutomatedCar auto){
+
+	}
+
     @Override
     public void loop() {
 
