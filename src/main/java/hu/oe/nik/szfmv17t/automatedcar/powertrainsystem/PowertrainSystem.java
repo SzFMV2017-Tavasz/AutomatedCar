4400bc1166ca604f13ed13f93a6c2ffcb09c60d7
Miklos Sebestyen <miklos.sebestyen@sap.com>
2017-04-03 17:30:01 +0200

    rebase merge

--- src/main/java/hu/oe/nik/szfmv17t/automatedcar/powertrainsystem/PowertrainSystem.java
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/powertrainsystem/PowertrainSystem.java
@@ -9,77 +9,76 @@ import hu.oe.nik.szfmv17t.physics.SteeringControl;
 
 public class PowertrainSystem extends SystemComponent {
 
-	// signal id table for PowertrainSystem
-	public static final int DEMO = 0;
-	public static final int SMI_BrakePedal = 10;
-	public static final int SMI_Gaspedal = 11;
-	public static final int SMI_Gear =  12;
-	public static final int SMI_SteeringWheel =  13;
-	public static final int ULTRASONIC_SENSOR_ID = 14;
-	public static final int Modelling = 20;
-	public static final int Physics = 30;
-	public static final int Physics_Speed =31;
-	public static final int Physics_Gear = 32;
-	public static final int Visualisation = 40;
+    // signal id table for PowertrainSystem
+    public static final int DEMO = 0;
+    public static final int SMI_BrakePedal = 10;
+    public static final int SMI_Gaspedal = 11;
+    public static final int SMI_Gear = 12;
+    public static final int SMI_SteeringWheel = 13;
+    public static final int ULTRASONIC_SENSOR_ID = 14;
+    public static final int Modelling = 20;
+    public static final int Physics = 30;
+    public static final int Physics_Speed = 31;
+    public static final int Physics_Gear = 32;
+    public static final int Visualisation = 40;
 
-	// physics
-	private SpeedControl speedControl;
-	private SteeringControl steeringControl;
+    // physics
+    private SpeedControl speedControl;
+    private SteeringControl steeringControl;
 
 
+    // input signals
+    private int gasPedal = 0;
+    private int brakePedal = 0;
 
-	// input signals
-	private int gasPedal = 0;
-	private int brakePedal = 0;
+    // Output signals
+    // Only these are available trough getters
+    private double steeringAngle = 0;
 
-	// Output signals
-	// Only these are available trough getters
-	private double steeringAngle = 0;
+    public PowertrainSystem(double height, double width, double carWeight) {
+        super();
 
-	public PowertrainSystem(double height, double width, double carWeight) {
-		super();
+        this.speedControl = new SpeedControl(carWeight);
+        this.steeringControl = new SteeringControl();
+    }
 
-		this.speedControl = new SpeedControl(carWeight);
-		this.steeringControl = new SteeringControl();
-	}
+    @Override
+    public void loop() {
+        //TODO write this
+    }
 
-	@Override
-	public void loop() {
-		//TODO write this
-	}
+    @Override
+    public void receiveSignal(Signal s) {
+        switch (s.getId()) {
+            // Handle demo signal
+            case SMI_BrakePedal:
+                int brakePedal = (int) s.getData();
+                this.speedControl.setBrakePedal(brakePedal);
+                break;
+            case SMI_Gaspedal:
+                int gasPedal = (int) s.getData();
+                this.speedControl.setGasPedal(gasPedal);
+                break;
+            case SMI_Gear:
+                AutoGearStates gear = AutoGearStates.values()[(int) s.getData()];
+                this.speedControl.setAutoGearState(gear);
+                break;
+            case SMI_SteeringWheel:
+                this.steeringAngle = this.steeringControl.calculateSteeringAngle((int) s.getData());
+                break;
+            case ULTRASONIC_SENSOR_ID:
+                System.out.println("Ultrasonic sensor: " + s.getData());
+                break;
+            default:
+                // ignore other signals
+        }
+    }
 
-	@Override
-	public void receiveSignal(Signal s) {
-		switch(s.getId()) {
-		// Handle demo signal
-		case SMI_BrakePedal:
-			int brakePedal = (int) s.getData();
-			this.speedControl.setBrakePedal(brakePedal);
-			break;
-		case SMI_Gaspedal:
-			int gasPedal = (int) s.getData();
-			this.speedControl.setGasPedal(gasPedal);
-			break;
-		case SMI_Gear:
-			AutoGearStates gear = AutoGearStates.values()[(int) s.getData()];
-			this.speedControl.setAutoGearState(gear);
-			break;
-		case SMI_SteeringWheel:
-			this.steeringAngle = this.steeringControl.calculateSteeringAngle((int) s.getData());
-			break;
-			case ULTRASONIC_SENSOR_ID:
-				System.out.println("Ultrasonic sensor: " + s.getData());
-				break;
-		default:
-			// ignore other signals
-		}
-	}
+    public double getSteeringAngle() {
+        return steeringAngle;
+    }
 
-	public double getSteeringAngle() {
-		return steeringAngle;
-	}
-
-	public double getVelocity() {
-		return speedControl.calculateVelocity();
-	}
+    public double getVelocity() {
+        return speedControl.calculateVelocity();
+    }
 }
