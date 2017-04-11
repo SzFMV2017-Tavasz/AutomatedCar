72c85cf7d8ca4a995dac7afef8ae277d631b8d6f
dlac03 <dlac03@gmail.com>
2017-04-11 22:12:31 +0200

    removed unnecessary tests from code

--- src/test/java/hu/oe/nik/szfmv17t/visualisation/FrameComposerTest.java
+++ src/test/java/hu/oe/nik/szfmv17t/visualisation/FrameComposerTest.java
@@ -37,27 +37,6 @@ public class FrameComposerTest {
         List<CameraObject> ret = fc.composeFrame();
     }
 
-    /*
-    @Test
-    public void nonVisibleTest () {
-        Camera camera = new Camera();
-        AutomatedCar car = createCarAt(480, 800);
-
-        WorldObjectMock nonVisible = new WorldObjectMock(100, 1810, 10, 10);
-
-        List<IWorldObject> objects = new ArrayList<>();
-        objects.add (car);
-        objects.add (nonVisible);
-
-        FrameComposer fc = new FrameComposer(new WorldVisualizationMock(objects), camera);
-        fc.setCameraSize(200, 200);
-
-        List<CameraObject> ret = fc.composeFrame();
-
-        assertEquals (1, ret.size());
-    }
-    */
-
     @Test
     public void visibleTest () {
         Camera camera = new Camera();
@@ -76,35 +55,7 @@ public class FrameComposerTest {
 
         assertEquals (2, ret.size());
     }
-
-    /*
-    @Test
-    public void mixedVisibleTest () {
-        Camera camera = new Camera();
-        AutomatedCar car = createCarAt(500, 800);
-
-        WorldObjectMock visible     = new WorldObjectMock(500, 810, 10, 10);
-        WorldObjectMock visible2    = new WorldObjectMock(550, 700, 10, 10);
-        WorldObjectMock nonVisible  = new WorldObjectMock(100, 610, 10, 10);
-        WorldObjectMock nonVisible2 = new WorldObjectMock(0, 0, 10, 10);
-        WorldObjectMock nonVisible3 = new WorldObjectMock(710, 800, 10, 10);
-
-        List<IWorldObject> objects = new ArrayList<>();
-        objects.add (car);
-        objects.add (nonVisible);
-        objects.add (visible);
-        objects.add (nonVisible2);
-        objects.add (visible2);
-        objects.add (nonVisible3);
-
-        FrameComposer fc = new FrameComposer(new WorldVisualizationMock(objects), camera);
-        fc.setCameraSize(200, 200);
-
-        List<CameraObject> ret = fc.composeFrame();
-
-        assertEquals (3, ret.size());
-    }
-*/
+    
 
     private AutomatedCar createCarAt (int x, int y) {
         return new AutomatedCar(x, y,108,240,0d,0,"car_1_white.png",200d,0d,0d);
