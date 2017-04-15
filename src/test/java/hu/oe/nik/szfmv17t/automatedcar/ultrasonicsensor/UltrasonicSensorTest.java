cef588e4cc39d7f0d31404d4710528e753d1c6af
NyiroGabor <gabi108@gmail.com>
2017-04-15 17:16:21 +0200

    More tests!

--- src/test/java/hu/oe/nik/szfmv17t/automatedcar/ultrasonicsensor/UltrasonicSensorTest.java
+++ src/test/java/hu/oe/nik/szfmv17t/automatedcar/ultrasonicsensor/UltrasonicSensorTest.java
@@ -10,36 +10,32 @@ import static org.junit.Assert.*;
  */
 public class UltrasonicSensorTest {
 
-
+    private UltrasonicSensor ultrasonicSensor;
 
     @Before
     public void setUp() throws Exception {
-
+        ultrasonicSensor = new UltrasonicSensor(1,0,0,0);
     }
 
     @Test
     public void calculateCoordinates() throws Exception {
-
-    }
-
-    @Test
-    public void getCoordinates() throws Exception {
-
+        ultrasonicSensor.calculateCoordinates(1,0,0,0);
+        assertTrue(ultrasonicSensor.getCoordinates().getMainX()>0 && ultrasonicSensor.getCoordinates().getMainY()>0);
     }
 
     @Test
     public void getViewAngle() throws Exception {
-
+        assertTrue(ultrasonicSensor.getViewAngle() == 100);
     }
 
     @Test
     public void getViewLength() throws Exception {
-
+        assertTrue(ultrasonicSensor.getViewLength() == 3);
     }
 
     @Test
     public void getSensorNumber() throws Exception {
-
+        assertTrue(ultrasonicSensor.getSensorNumber() == 1);
     }
 
 }
