86ffca3816c006dc29bf8b80beb7cfc56f5370d8
pintergreg <pintergreg@gmail.com>
2017-04-18 09:36:11 +0200

    add headless protection for FramComposerTest

--- src/test/java/hu/oe/nik/szfmv17t/visualisation/FrameComposerTest.java
+++ src/test/java/hu/oe/nik/szfmv17t/visualisation/FrameComposerTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import hu.oe.nik.szfmv17t.visualisation.mock.WorldObjectMock;
 import hu.oe.nik.szfmv17t.visualisation.mock.WorldVisualizationMock;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,44 +17,50 @@ import static org.junit.Assert.*;
 public class FrameComposerTest {
     @Test
     public void frameComposeReturnsCar () {
-        AutomatedCar car = createCarAt(480, 800);
+        if (!GraphicsEnvironment.isHeadless()) {
+            AutomatedCar car = createCarAt(480, 800);
 
-        List<IWorldObject> objects = new ArrayList<>();
-        objects.add (car);
+            List<IWorldObject> objects = new ArrayList<>();
+            objects.add(car);
 
-        FrameComposer fc = new FrameComposer(new WorldVisualizationMock(objects), new Camera());
+            FrameComposer fc = new FrameComposer(new WorldVisualizationMock(objects), new Camera());
 
-        List<CameraObject> ret = fc.composeFrame();
+            List<CameraObject> ret = fc.composeFrame();
 
-        assertEquals(car, ret.get(0).getWorldObject());
+            assertEquals(car, ret.get(0).getWorldObject());
+        }
     }
 
     @Test(expected = CarNotFoundException.class)
     public void frameComposeThrowsException () {
-        List<IWorldObject> objects = new ArrayList<>();
+        if (!GraphicsEnvironment.isHeadless()) {
+            List<IWorldObject> objects = new ArrayList<>();
 
-        FrameComposer fc = new FrameComposer(new WorldVisualizationMock(objects), new Camera());
+            FrameComposer fc = new FrameComposer(new WorldVisualizationMock(objects), new Camera());
 
-        List<CameraObject> ret = fc.composeFrame();
+            List<CameraObject> ret = fc.composeFrame();
+        }
     }
 
     @Test
     public void visibleTest () {
-        Camera camera = new Camera();
-        AutomatedCar car = createCarAt(480, 800);
+        if (!GraphicsEnvironment.isHeadless()) {
+            Camera camera = new Camera();
+            AutomatedCar car = createCarAt(480, 800);
 
-        WorldObjectMock visible = new WorldObjectMock(500, 810, 10, 10);
+            WorldObjectMock visible = new WorldObjectMock(500, 810, 10, 10);
 
-        List<IWorldObject> objects = new ArrayList<>();
-        objects.add (car);
-        objects.add (visible);
+            List<IWorldObject> objects = new ArrayList<>();
+            objects.add(car);
+            objects.add(visible);
 
-        FrameComposer fc = new FrameComposer(new WorldVisualizationMock(objects), camera);
-        fc.setCameraSize(200, 200);
+            FrameComposer fc = new FrameComposer(new WorldVisualizationMock(objects), camera);
+            fc.setCameraSize(200, 200);
 
-        List<CameraObject> ret = fc.composeFrame();
+            List<CameraObject> ret = fc.composeFrame();
 
-        assertEquals (2, ret.size());
+            assertEquals(2, ret.size());
+        }
     }
     
 
