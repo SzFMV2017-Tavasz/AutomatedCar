613e4c90471d78082a9ac1d7ab0de680b19f8132
Gellert Babel <gelli.babel94@gmail.com>
2017-04-24 23:04:54 +0200

    Opening sort method to public, anyone can use it.
    For the front we have pre builded rectangle crafter and method that calls.

--- src/main/java/hu/oe/nik/szfmv17t/environment/utils/InWay.java
+++ src/main/java/hu/oe/nik/szfmv17t/environment/utils/InWay.java
@@ -18,42 +18,47 @@ import java.util.List;
  * @author Gellert Babel <OE-NIK>
  */
 public class InWay {
-    private static Rectangle2D.Double RectangleCrafter(IWorldObject obj){
-        Point2D.Double car0=new Point2D.Double(obj.getCenterX()-obj.getWidth()/2, obj.getCenterY()-obj.getHeight()/2);
-        Point2D.Double car1=new Point2D.Double(obj.getCenterX()+obj.getWidth()/2, obj.getCenterY()-obj.getHeight()/2);
-        Point2D.Double mid=midPoint(car0, car1);
-        AffineTransform transform = AffineTransform.getRotateInstance(obj.getAxisAngle(),mid.getX(),mid.getY());
+
+    private static Rectangle2D.Double RectangleCrafterFront(IWorldObject obj) {
+        Point2D.Double car0 = new Point2D.Double(obj.getCenterX() - obj.getWidth() / 2, obj.getCenterY() - obj.getHeight() / 2);
+        Point2D.Double car1 = new Point2D.Double(obj.getCenterX() + obj.getWidth() / 2, obj.getCenterY() - obj.getHeight() / 2);
+        Point2D.Double mid = midPoint(car0, car1);
+        AffineTransform transform = AffineTransform.getRotateInstance(obj.getAxisAngle(), mid.getX(), mid.getY());
         transform.transform(car0, car0);
         transform.transform(car1, car1);
-        Point2D.Double far0=new Point2D.Double(obj.getCenterX()-obj.getWidth()/2, obj.getCenterY()-10e5);
-        Point2D.Double far1=new Point2D.Double(obj.getCenterX()+obj.getWidth()/2, obj.getCenterY()-10e5);
+        Point2D.Double far0 = new Point2D.Double(obj.getCenterX() - obj.getWidth() / 2, obj.getCenterY() - 10e5);
+        Point2D.Double far1 = new Point2D.Double(obj.getCenterX() + obj.getWidth() / 2, obj.getCenterY() - 10e5);
         transform.transform(far0, far0);
         transform.transform(far1, far1);
-        Rectangle2D.Double r= new Rectangle2D.Double();
+        Rectangle2D.Double r = new Rectangle2D.Double();
         r.add(car0);
         r.add(car1);
         r.add(far0);
         r.add(far1);
         return r;
     }
-    private static Point2D.Double midPoint(Point2D.Double p,Point2D.Double o) {
-     double mx = (p.x + o.x)/2;
-     double my = (p.y + o.y)/2;
-     return new Point2D.Double(mx,my);
-}
-    public static List<IWorldObject> InWaySortCarFront(AutomatedCar car,List<IWorldObject> objectsin){
-        List<IWorldObject> returnL= new ArrayList<>();
-        Rectangle2D.Double carRect= RectangleCrafter(car);
-        for(IWorldObject i:objectsin){
-            if(IsPointInRect(carRect,new Point2D.Double(i.getCenterX(),i.getCenterY()))){
-                returnL.add(i);
-            }
-        }
+
+    private static Point2D.Double midPoint(Point2D.Double p, Point2D.Double o) {
+        double mx = (p.x + o.x) / 2;
+        double my = (p.y + o.y) / 2;
+        return new Point2D.Double(mx, my);
+    }
+
+    public static List<IWorldObject> InWaySortCarFront(AutomatedCar car, List<IWorldObject> objectsin) {
+        Rectangle2D.Double carRect = RectangleCrafterFront(car);
+        return SortingMethod(carRect, objectsin);
+    }
+
+    public static List<IWorldObject> SortingMethod(Rectangle2D.Double areaRect, List<IWorldObject> objectsin) {
+        List<IWorldObject> returnL = new ArrayList<>();
+        objectsin.stream().filter((i) -> (IsPointInRect(areaRect, new Point2D.Double(i.getCenterX(), i.getCenterY())))).forEachOrdered((i) -> {
+            returnL.add(i);
+        });
         return returnL;
     }
-    private static Boolean IsPointInRect(Rectangle2D.Double In, Point2D.Double P){
+
+    private static Boolean IsPointInRect(Rectangle2D.Double In, Point2D.Double P) {
         return In.contains(P);
     }
 
-    
 }
