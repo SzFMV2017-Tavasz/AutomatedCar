a3b8ec0c0a19cebd463765c12c2fcf5391ade5f0
karpatidaniel <dani.karpati92@gmail.com>
2017-04-25 22:36:19 +0200

    new implementation of determining car's lane

--- src/main/java/hu/oe/nik/szfmv17t/automatedcar/radarsensor/RadarController.java
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/radarsensor/RadarController.java
@@ -5,32 +5,90 @@ import hu.oe.nik.szfmv17t.automatedcar.SystemComponent;
 import hu.oe.nik.szfmv17t.automatedcar.bus.Signal;
 import hu.oe.nik.szfmv17t.automatedcar.bus.VirtualFunctionBus;
 import hu.oe.nik.szfmv17t.automatedcar.powertrainsystem.PowertrainSystem;
+import hu.oe.nik.szfmv17t.environment.interfaces.IWorldObject;
+import hu.oe.nik.szfmv17t.environment.utils.Position;
+import hu.oe.nik.szfmv17t.environment.utils.Resizer;
+import hu.oe.nik.szfmv17t.environment.utils.Triangle;
+import hu.oe.nik.szfmv17t.environment.domain.World;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RadarController extends SystemComponent{
-	private static RadarSensor radarSensor;
+	private Resizer resizer;
+	private RadarSensor radarSensor;
 	private AutomatedCar automatedCar;
-    
-	public RadarController (AutomatedCar auto) {
-		this.automatedCar = auto;
+	private World world;
+	private List<IWorldObject> detectedObjects;
+	private List<IWorldObject> allObjectsInCarLane;
+
+	public RadarController (AutomatedCar car,World world) {
+		resizer = Resizer.getResizer();
+		this.automatedCar = car;
+		this.world = world;
+		detectedObjects = new ArrayList<IWorldObject>();
+		allObjectsInCarLane = new ArrayList<IWorldObject>();
 		initSensor();
 	}
 
 	private void initSensor(){
-		radarSensor = new RadarSensor(automatedCar.getCenterX(),automatedCar.getCenterY(), automatedCar.getAxisAngle());
+		radarSensor = new RadarSensor();
+	}
+    
+	private IWorldObject getClosestObject(List<IWorldObject> allObjectsInCarLane){
+		double mindistance=Double.MAX_VALUE;
+		IWorldObject closestIWorldObject=allObjectsInCarLane.get(0);
+		for (IWorldObject currIWorldObject : allObjectsInCarLane) {
+			Point wObjCenter=new Point((int)currIWorldObject.getCenterX(),(int)currIWorldObject.getCenterY());
+			double currDistance=wObjCenter.distance(automatedCar.getCenterX(),automatedCar.getCenterY());
+			if(currDistance<mindistance){
+				mindistance=currDistance;
+				closestIWorldObject=currIWorldObject;
+			}			
+		}
+		return closestIWorldObject;
+	}
+    @Override
+    public void loop(){
+        VirtualFunctionBus.sendSignal(new Signal(PowertrainSystem.RADAR_SENSOR_ID, null));
+        Triangle sensorArea = radarSensor.calculateCoordinates(automatedCar.getPositionObj(), -automatedCar.getAxisAngle());
+		detectedObjects = world.checkSensorArea(sensorArea);
+		allObjectsInCarLane=radarSensor.selectObjectsInCarLane(detectedObjects, automatedCar.getPositionObj(),-automatedCar.getAxisAngle());
+		logInformationOfDetectedObjectsByRadarSensor();
+		logAllObjectsInCarLane();
+		logClosestObjectInCarLane();
+	}
+
+	private void logInformationOfDetectedObjectsByRadarSensor(){
+		System.out.println("\nDetected objects by the radar sensor: ");
+		for (IWorldObject obj: detectedObjects) {
+			Point detectedObject = new Point((int)obj.getCenterX(),(int)obj.getCenterY());
+			System.out.println(obj.getClass().getSimpleName() + " " + obj.getImageName() + " X:" + obj.getCenterX() + " Y:" + obj.getCenterY() + ", distance from car center: " + resizer.coordinateToMeter(detectedObject.distance(automatedCar.getCenterX(),automatedCar.getCenterY()))+ " m");
+		}
+	}
+	
+	private void logAllObjectsInCarLane(){
+		System.out.println("\nOBJECTS IN CAR LANE ");
+		for (IWorldObject obj: allObjectsInCarLane) {
+			Point detectedObject = new Point((int)obj.getCenterX(),(int)obj.getCenterY());
+			System.out.println("x:"+automatedCar.getCenterX()+" Y:"+automatedCar.getCenterY()+" "+ obj.getClass().getSimpleName() + " " + obj.getImageName() + " X:" + obj.getCenterX() + " Y:" + obj.getCenterY() + ", distance from car center: " + resizer.coordinateToMeter(detectedObject.distance(automatedCar.getCenterX(),automatedCar.getCenterY()))+ " m");
+		}
+	}
+	
+	private void logClosestObjectInCarLane(){
+		System.out.println("\nCLOSEST OBJECT IN CAR LANE ");
+		if(!allObjectsInCarLane.isEmpty())
+		{
+			IWorldObject theclosestObj= getClosestObject(allObjectsInCarLane);
+			if(theclosestObj!=null){
+				Point objCenter=new Point((int)theclosestObj.getCenterX(),(int)theclosestObj.getCenterY());
+				System.out.print(theclosestObj.getImageName()+" "+objCenter.getClass().getSimpleName()+ " distance from car center: ");
+				System.out.format("%.2f m%n",  resizer.coordinateToMeter(objCenter.distance(automatedCar.getCenterX(),automatedCar.getCenterY())));				
+			}
+		}
 	}
 
     @Override
-    public void loop() {
-            VirtualFunctionBus.sendSignal(new Signal(PowertrainSystem.RADAR_SENSOR_ID, null));
-            radarSensor.calculateCoordinates(automatedCar.getAxisAngle(), automatedCar.getCenterX(), automatedCar.getCenterY());
-    }
-
-    @Override
-    public void receiveSignal(Signal s) {
-
-    }
-
-    public static RadarSensor getRadarSensor(){
-        return radarSensor;
-    }
+    public void receiveSignal(Signal s) { }
 }
--- src/main/java/hu/oe/nik/szfmv17t/automatedcar/radarsensor/RadarSensor.java
+++ src/main/java/hu/oe/nik/szfmv17t/automatedcar/radarsensor/RadarSensor.java
@@ -1,71 +1,132 @@
 package hu.oe.nik.szfmv17t.automatedcar.radarsensor;
 
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
-import hu.oe.nik.szfmv17t.environment.utils.Resizer;
-import hu.oe.nik.szfmv17t.environment.utils.SensorType;
-import hu.oe.nik.szfmv17t.environment.utils.Triangle;
+import hu.oe.nik.szfmv17t.environment.interfaces.IWorldObject;
+import hu.oe.nik.szfmv17t.environment.utils.*;
 
 public class RadarSensor {
-	private double viewLength;
-	private double viewAngle;
-	private double sensorDistanceFromCenter;
+	static private final double VIEW_LENGTH_IN_METER = 200;
+	static private final double VIEW_ANGLE_IN_DEGREE = 60;
+	static private final double VIEW_ANGLE_IN_RADIAN = Math.toRadians(VIEW_ANGLE_IN_DEGREE);
 	private Resizer resizer;
 	private double viewLengthInCoordinates;
-	private double sensorTrianglePointsDistanceFromSensor;
-	private RadarSensorCoordinates coordinates;
-	
-	public RadarSensor(double carMainCoordinateX, double carMainCoordinateY, double carAxisAngle){
+	private double triangleAdjacentSideLength;
+
+	public RadarSensor(){
 		resizer = Resizer.getResizer();
-		sensorDistanceFromCenter = 127;
-		viewLength = 200;
-		viewAngle = 60;
-		viewLengthInCoordinates = resizer.meterToCoordinate(viewLength);
-		sensorTrianglePointsDistanceFromSensor = sensorTriangleBasicCalculation();
-		coordinates = new RadarSensorCoordinates();
-		calculateCoordinates(carAxisAngle, carMainCoordinateX, carMainCoordinateY);
-	}	
-	private double sensorTriangleBasicCalculation() {
-		return viewLengthInCoordinates / Math.cos(viewAngle/2);
-	}
-	public void calculateCoordinates(double carAxisAngle, double carMainCoordinateX, double carMainCoordinateY) {
-		double sensorAngle = carAxisAngle;
-		sensorPositionCalculate(sensorAngle, carMainCoordinateX, carMainCoordinateY);
-		sensorPointsCalculate(carAxisAngle);
+		viewLengthInCoordinates = resizer.meterToCoordinate(VIEW_LENGTH_IN_METER);
+		triangleAdjacentSideLength = calculateAdjacentSideLength();
 	}
 
-	private void sensorPositionCalculate(double angleFROMCarAxisAngle, double carMainCoordinateX, double carMainCoordinateY) {
-		double x = Math.sin(angleFROMCarAxisAngle * (Math.PI / 180)) * sensorDistanceFromCenter;
-		double y = Math.cos(angleFROMCarAxisAngle * (Math.PI / 180)) * sensorDistanceFromCenter;
-		coordinates.setMainCoordinates(carMainCoordinateX + x, carMainCoordinateY + y);
+	private double calculateAdjacentSideLength() {
+		return Math.tan(VIEW_ANGLE_IN_RADIAN / 2.0) * viewLengthInCoordinates;
 	}
 
-	private void sensorPointsCalculate(double carAxisAngle) {
-		double halfViewAngle = viewAngle / 2;
-		double leftX = Math.sin((carAxisAngle - halfViewAngle) * (Math.PI / 180)) * sensorTrianglePointsDistanceFromSensor;
-		double leftY = Math.cos((carAxisAngle - halfViewAngle) * (Math.PI / 180)) * sensorTrianglePointsDistanceFromSensor;
-		double rightX = Math.sin((carAxisAngle + halfViewAngle) * (Math.PI / 180)) * sensorTrianglePointsDistanceFromSensor;
-		double rightY = Math.cos((carAxisAngle + halfViewAngle) * (Math.PI / 180)) * sensorTrianglePointsDistanceFromSensor;
-		coordinates.setLeftCoordinates(leftX + coordinates.getMainX(), leftY + coordinates.getMainY());
-		coordinates.setRightCoordinates(rightX + coordinates.getMainX(), rightY + coordinates.getMainY());
+	public Triangle calculateCoordinates(Position carPosition, double carAxisAngle) {
+		Point defaultSensorCoordinate = new Point((int)carPosition.getCenter().getX(), (int)carPosition.getMinimumY());
+		Point rotatedSensorCoordinate = rotatePoint(carPosition.getCenter().getX(),carPosition.getCenter().getY(),carAxisAngle, defaultSensorCoordinate);
+
+		Point defaultLeftCoordinate = new Point((int)(carPosition.getCenter().getX()-triangleAdjacentSideLength), (int)(defaultSensorCoordinate.getY()-viewLengthInCoordinates));
+		Point rotatedLeftCoordinate = rotatePoint(carPosition.getCenter().getX(),carPosition.getCenter().getY(),carAxisAngle,defaultLeftCoordinate);
+
+		Point defaultRightCoordinate = new Point((int)(carPosition.getCenter().getX()+triangleAdjacentSideLength), (int)(defaultSensorCoordinate.getY()-viewLengthInCoordinates));
+		Point rotatedRightCoordinate = rotatePoint(carPosition.getCenter().getX(),carPosition.getCenter().getY(),carAxisAngle,defaultRightCoordinate);
+
+		return new Triangle(rotatedSensorCoordinate,rotatedLeftCoordinate,rotatedRightCoordinate,SensorType.Radar);
 	}
 
-	public RadarSensorCoordinates getCoordinates() {
-		return coordinates;
+	private Point rotatePoint(double centerX, double centerY, double angle, Point pointToTurn){
+		double newX = Math.cos(angle) * (pointToTurn.getX() - centerX) - Math.sin(angle) * (pointToTurn.getY() - centerY) + centerX;
+		double newY = Math.sin(angle) * (pointToTurn.getX() - centerX) + Math.cos(angle) * (pointToTurn.getY() - centerY) + centerY;
+		return new Point((int)newX,(int)newY);
+	}
+	
+	public List<IWorldObject> selectObjectsInCarLane(List<IWorldObject> allDetectedObjects,Position carPosition, double carAxisAngle){
+		Point lineEndpoints[]=calcLineEndpoints(carPosition,carAxisAngle);
+		double pointA[]={lineEndpoints[0].getX(),lineEndpoints[0].getY()};
+		double pointB[]={lineEndpoints[1].getX(),lineEndpoints[1].getY()};
+		double laneWidthInMapUnits=resizer.meterToCoordinate(1.5);
+		
+		List<IWorldObject> objectsInCarLane=new ArrayList<IWorldObject>();
+		
+		for (IWorldObject currIWorldObject : allDetectedObjects) {
+			double pointC[]={currIWorldObject.getCenterX(),currIWorldObject.getCenterY()};
+			double detectedObjectCenterDistance=LineToPointDistance2D(pointA, pointB, pointC, false);
+			if(detectedObjectCenterDistance<laneWidthInMapUnits)
+			{
+				objectsInCarLane.add(currIWorldObject);
+			}
+		}
+		return objectsInCarLane;
+	}
+	
+	private Point[] calcLineEndpoints(Position carPosition, double carAxisAngle){
+		
+		Point defaultStartPoint = new Point((int)carPosition.getCenter().getX(), (int)carPosition.getMinimumY());
+		Point rotatedStartPoint = rotatePoint(carPosition.getCenter().getX(),carPosition.getCenter().getY(),carAxisAngle, defaultStartPoint);
+		Point defaultEndPoint = new Point((int)(carPosition.getCenter().getX()), (int)(defaultStartPoint.getY()-viewLengthInCoordinates));
+		Point rotatedEndPoint = rotatePoint(carPosition.getCenter().getX(),carPosition.getCenter().getY(),carAxisAngle, defaultEndPoint);
+		
+		Point lineEndpoints[]= {rotatedStartPoint,rotatedEndPoint};
+		return lineEndpoints;
+	}
+	
+	//Compute the dot product AB . AC
+	private double DotProduct(double[] pointA, double[] pointB, double[] pointC)
+	{
+	    double[] AB = new double[2];
+	    double[] BC = new double[2];
+	    AB[0] = pointB[0] - pointA[0];
+	    AB[1] = pointB[1] - pointA[1];
+	    BC[0] = pointC[0] - pointB[0];
+	    BC[1] = pointC[1] - pointB[1];
+	    double dot = AB[0] * BC[0] + AB[1] * BC[1];
+
+	    return dot;
 	}
 
-	public double getViewAngle() {
-		return viewAngle;
+	//Compute the cross product AB x AC
+	private double CrossProduct(double[] pointA, double[] pointB, double[] pointC)
+	{
+	    double[] AB = new double[2];
+	    double[] AC = new double[2];
+	    AB[0] = pointB[0] - pointA[0];
+	    AB[1] = pointB[1] - pointA[1];
+	    AC[0] = pointC[0] - pointA[0];
+	    AC[1] = pointC[1] - pointA[1];
+	    double cross = AB[0] * AC[1] - AB[1] * AC[0];
+
+	    return cross;
 	}
 
-	public double getViewLength() {
-		return viewLength;
+	//Compute the distance from A to B
+	double Distance(double[] pointA, double[] pointB)
+	{
+	    double d1 = pointA[0] - pointB[0];
+	    double d2 = pointA[1] - pointB[1];
+
+	    return Math.sqrt(d1 * d1 + d2 * d2);
 	}
 
-	public Triangle getSensorViewTriangle() {
-		Point leftCord = new Point((int) coordinates.getLeftX(), (int) coordinates.getLeftY());
-		Point rightCord = new Point((int) coordinates.getRightX(), (int) coordinates.getRightY());
-		Point mainCore = new Point((int) coordinates.getMainX(), (int) coordinates.getMainY());
-		return new Triangle(leftCord, rightCord, mainCore, SensorType.Radar);
-	}
+	//Compute the distance from AB to C
+	//if isSegment is true, AB is a segment, not a line.
+	double LineToPointDistance2D(double[] pointA, double[] pointB, double[] pointC, 
+	    boolean isSegment)
+	{
+	    double dist = CrossProduct(pointA, pointB, pointC) / Distance(pointA, pointB);
+	    if (isSegment)
+	    {
+	        double dot1 = DotProduct(pointA, pointB, pointC);
+	        if (dot1 > 0) 
+	            return Distance(pointB, pointC);
+
+	        double dot2 = DotProduct(pointB, pointA, pointC);
+	        if (dot2 > 0) 
+	            return Distance(pointA, pointC);
+	    }
+	    return Math.abs(dist);
+	} 
 }
